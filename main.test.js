de05fb04523a606dd1132670779567735addd1f6
moazzemhosen <92745004+moazzemhosen@users.noreply.github.com>
2022-07-08 00:22:58 +0530

    done
    done

--- main.test.js
+++ main.test.js
@@ -1,7 +1,23 @@
-const sum = require("./main");
+const timeConverter = require("./main");
 
-describe("Testing sum function", () => {
-  test("add n possitive number", () => {
-    expect(sum(1, 4, 3, 4)).toBe(12);
+describe("testing timeConverter function", () => {
+  test("should take ms and return a string", () => {
+    expect(timeConverter(21000)).toBe("21 : seconds");
+  });
+
+  test("should take ms as string  and return a string", () => {
+    expect(timeConverter("21000")).toBe("21 : seconds");
+  });
+
+  test("should take ms sec is singular should return insingular and return a string", () => {
+    expect(timeConverter(1000)).toBe("1 : second");
+  });
+
+  test("should take ms if after converting everything is singular then return everything as a singular  string", () => {
+    expect(timeConverter(90061000)).toBe("1 : day,1 : hour,1 : minute,1 : second");
+  });
+
+  test("should take ms if after converting everything is prural then return everything as a prural  string", () => {
+    expect(timeConverter(180122000)).toBe("2 : days,2 : hours,2 : minutes,2 : seconds");
   });
 });
